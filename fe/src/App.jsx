32dfc09e561826437e7a079fcbcebf5beef4f3cf
Nevy <navyyaseen12@gmail.com>
2025-09-14 23:24:59 +0500

    App.jsx changes

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="p-4 mx-auto sm:max-w-xl md:max-w-2xl lg:max-w-3xl xl:max-w-6xl">
       <Navbar />
+      test test test test test test test test test test test test test test test
       Here is our main content Here is our main content Here is our main
       contentHere is our main contentHere is our main contentHere is our main
       contentHere is our main contentHere is our main contentHere is our main
